ace2d1431009f62b48415aa21d299976d5e9bdc5
aishuduth2000-umaguru <61201659+aishuduth2000-umaguru@users.noreply.github.com>
2020-04-15 08:05:09 +0530

    Add files via upload

---
+++ localhost.sql
@@ -0,0 +1,296 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 15, 2020 at 02:31 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id12987205_salon`
+--
+CREATE DATABASE IF NOT EXISTS `id12987205_salon` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `id12987205_salon`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` char(50) DEFAULT NULL,
+  `UserName` char(50) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Aishu', 'admin', 7815823757, 'tester1@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-07-25 06:21:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblappointment`
+--
+
+CREATE TABLE `tblappointment` (
+  `ID` int(10) NOT NULL,
+  `AptNumber` varchar(80) DEFAULT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `PhoneNumber` bigint(11) DEFAULT NULL,
+  `AptDate` varchar(120) DEFAULT NULL,
+  `AptTime` varchar(120) DEFAULT NULL,
+  `Services` varchar(120) DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(250) DEFAULT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `RemarkDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblappointment`
+--
+
+INSERT INTO `tblappointment` (`ID`, `AptNumber`, `Name`, `Email`, `PhoneNumber`, `AptDate`, `AptTime`, `Services`, `ApplyDate`, `Remark`, `Status`, `RemarkDate`) VALUES
+(13, '675849597', 'chandan', 'mck@gmail.com', 1234567890, '3/30/2020', '1:30am', 'Fruit Facial', '2020-03-22 08:07:13', 'Chandan', '1', '2020-03-22 08:07:59'),
+(19, '897356566', 'CHandaishu', 'chandaishu@gmail.com', 7815823757, '4/30/2020', '1:00am', 'Eye Brows', '2020-04-10 10:22:05', 'hello', '1', '2020-04-10 10:27:13'),
+(21, '874267834', 'Aishu', 'aishuduthg26@gmail.com', 9064679466, '4/8/2020', '1:00am', 'Layer Haircut', '2020-04-11 07:51:16', NULL, NULL, '0000-00-00 00:00:00'),
+(22, '215512499', 'Aishu', 'aishuduthg26@gmail.com', 9985634523, '4/8/2020', '1:00am', 'Layer Haircut', '2020-04-11 07:51:55', NULL, NULL, '0000-00-00 00:00:00'),
+(23, '638670960', 'varsha DUTH G', 'varshaduth26@gmail.com', 9908764356, '14/4/2020', '2:00 pm', 'U-Shape Hair Cut', '2020-04-13 06:06:22', NULL, NULL, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomers`
+--
+
+CREATE TABLE `tblcustomers` (
+  `ID` int(10) NOT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `Gender` enum('Female','Male','Transgender') DEFAULT NULL,
+  `Details` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcustomers`
+--
+
+INSERT INTO `tblcustomers` (`ID`, `Name`, `Email`, `MobileNumber`, `Gender`, `Details`, `CreationDate`, `UpdationDate`) VALUES
+(6, 'Chandan', 'mckchandan1999@gmail.com', 7815823757, 'Female', 'call me ChinnuChandu', '2020-03-24 07:08:11', NULL),
+(7, 'Chandaishu', 'chandaishu@gmail.com', 7815823757, 'Female', 'I Need a kiss ', '2020-04-10 10:30:06', NULL),
+(8, 'Aishu', 'aishuduth@gmail.com', 9036574711, 'Female', 'VERY nice', '2020-04-10 10:30:15', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblinvoice`
+--
+
+CREATE TABLE `tblinvoice` (
+  `id` int(11) NOT NULL,
+  `Userid` int(11) DEFAULT NULL,
+  `ServiceId` int(11) DEFAULT NULL,
+  `BillingId` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblinvoice`
+--
+
+INSERT INTO `tblinvoice` (`id`, `Userid`, `ServiceId`, `BillingId`, `PostingDate`) VALUES
+(1, 2, 2, 621839533, '2018-07-30 15:33:22'),
+(2, 2, 5, 621839533, '2019-06-04 15:33:22'),
+(3, 2, 6, 621839533, '2019-07-30 15:33:22'),
+(4, 2, 7, 621839533, '2019-07-30 15:33:22'),
+(5, 1, 1, 904156433, '2019-07-30 15:40:42'),
+(6, 1, 2, 904156433, '2019-07-30 15:40:42'),
+(7, 1, 3, 904156433, '2019-07-30 15:40:42'),
+(8, 1, 4, 904156433, '2019-07-30 15:40:42'),
+(9, 3, 1, 225057023, '2019-07-30 16:03:32'),
+(10, 3, 8, 225057023, '2019-07-30 16:03:32'),
+(11, 3, 1, 970548035, '2019-07-31 04:42:45'),
+(12, 3, 6, 970548035, '2019-07-31 04:42:45'),
+(13, 3, 9, 970548035, '2019-07-31 04:42:45'),
+(14, 4, 2, 942476283, '2019-08-19 13:39:13'),
+(15, 4, 12, 942476283, '2019-08-19 13:39:13'),
+(16, 5, 3, 297018570, '2019-08-21 16:25:27'),
+(17, 5, 4, 297018570, '2019-08-21 16:25:27'),
+(18, 5, 8, 297018570, '2019-08-21 16:25:27'),
+(19, 6, 2, 903955120, '2020-03-24 07:09:43'),
+(20, 6, 5, 903955120, '2020-03-24 07:09:43'),
+(21, 6, 9, 903955120, '2020-03-24 07:09:43'),
+(22, 6, 15, 903955120, '2020-03-24 07:09:43'),
+(23, 6, 16, 903955120, '2020-03-24 07:09:43'),
+(24, 8, 4, 822738775, '2020-04-10 10:31:52'),
+(25, 8, 8, 822738775, '2020-04-10 10:31:52'),
+(26, 8, 12, 822738775, '2020-04-10 10:31:52'),
+(27, 7, 16, 949285273, '2020-04-10 10:31:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL,
+  `Timing` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `Timing`) VALUES
+(1, 'aboutus', 'About Us', '        Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart from regular bleachings and Facials, many types of hairstyles, Bridal and cine make-up and different types of Facials &amp; fashion hair colourings.', NULL, NULL, NULL, ''),
+(2, 'contactus', 'Contact Us', '                Udagygiri , Mysuru', 'chandaishu@gmail.com', 7815823757, NULL, '10:30 am to 7:30 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblservices`
+--
+
+CREATE TABLE `tblservices` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `Cost` int(10) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblservices`
+--
+
+INSERT INTO `tblservices` (`ID`, `ServiceName`, `Cost`, `CreationDate`) VALUES
+(1, 'O3 Facial', 1200, '2019-07-25 11:22:38'),
+(2, 'Fruit Facial', 500, '2019-07-25 11:22:53'),
+(3, 'Charcol Facial', 1000, '2019-07-25 11:23:10'),
+(4, 'Deluxe Menicure', 500, '2019-07-25 11:23:34'),
+(5, 'Deluxe Pedicure', 600, '2019-07-25 11:23:47'),
+(6, 'Normal Menicure', 300, '2019-07-25 11:24:01'),
+(7, 'Normal Pedicure', 400, '2019-07-25 11:24:19'),
+(8, 'U-Shape Hair Cut', 250, '2019-07-25 11:24:38'),
+(9, 'Layer Haircut', 550, '2019-07-25 11:24:53'),
+(10, 'Rebonding', 3999, '2019-07-25 11:25:08'),
+(11, 'Loreal Hair Color(Full)', 1200, '2019-07-25 11:25:35'),
+(12, 'Body Spa', 1500, '2019-08-19 13:36:27'),
+(16, 'Eye Brows', 100, '2020-03-23 05:51:17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcustomers`
+--
+ALTER TABLE `tblcustomers`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblinvoice`
+--
+ALTER TABLE `tblinvoice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `tblcustomers`
+--
+ALTER TABLE `tblcustomers`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblinvoice`
+--
+ALTER TABLE `tblinvoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
